1a6fe707d94be8886156c58aa4815ddd4bb7ae27
CaelumD25 <caelumdudek25@gmail.com>
2022-09-19 15:32:16 -0700

    Creating the template for the scraping logic

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-from geopy.geocoders import bing
+import time
+
+from scrape_url import scrape
 
 from API import kijiji as k
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("Hello World")
+    start_time = time
+    for x in scrape("https://www.kijiji.ca/b-real-estate/victoria-bc/c34l1700173?address=MacLaurin+Building%2C+Victoria%2C+BC+V8P+5C2%2C+Canada&ll=48.462368,-123.313803"):
+        print(x)
+    print(time-start_time)
     #geolocator = bing.Bing("AkVuw-CNwasVEvDE8fqHd7qZ2YDO1UTxjkB9UEW7AfKWCJCJpKqInXb83YBH5EoI")
     #k.Unit().digest_url("https://www.kijiji.ca/v-apartments-condos/nelson/beasley-two-bedroom-apartment-with-a-view/1631541147?undefined", geolocator)
 
--- scrape_url.py
+++ scrape_url.py
@@ -1,9 +1,12 @@
 import re
 from bs4 import BeautifulSoup
 import requests
+import API.kijiji as k
+from geopy.geocoders import bing
 
 def match_url(url):
-    if re.match(url, r"(https://www\.kijiji\.ca).*"):
+    return 1
+    if re.match(url, r"^(https://www\.kijiji\.ca).*"):
         print("Kijiji Found")
         return 1
     elif re.match(url, r"https://.*?\.(craigslist.org).*"):
@@ -21,9 +24,10 @@ def match_url(url):
 
 def scrape_kijiji(url):
     requested_html = requests.get(url).text
-    soup = BeautifulSoup()
-    pass
-
+    soup = BeautifulSoup(requested_html, "html.parser")
+    listings = soup.select("div.info-container")
+    results = ["https://www.kijiji.ca" + listing.select("a.title")[0]["href"] for listing in listings]
+    return results
 
 def scrape_craigslist(url):
     pass
@@ -38,9 +42,11 @@ def scrape_fb(url):
 
 
 def scrape(url: str):
+    geolocator = bing.Bing("AkVuw-CNwasVEvDE8fqHd7qZ2YDO1UTxjkB9UEW7AfKWCJCJpKqInXb83YBH5EoI")
     match match_url(url):
         case 1:
-            scrape_kijiji(url)
+            listings = scrape_kijiji(url)
+            return [k.Unit(listing, geolocator) for listing in listings]
         case 2:
             scrape_craigslist(url)
         case 3:
