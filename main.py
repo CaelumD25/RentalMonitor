dd93c4b247473b85a3ee7cb5e9ea4b3c7821ca48
CaelumD25 <caelumdudek25@gmail.com>
2022-09-19 13:39:26 -0700

    Creating the template for the scraping logic

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from API import kijiji as k
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("Hello World")
-    geolocator = bing.Bing("AkVuw-CNwasVEvDE8fqHd7qZ2YDO1UTxjkB9UEW7AfKWCJCJpKqInXb83YBH5EoI")
-    k.Unit().digest_url("https://www.kijiji.ca/v-apartments-condos/nelson/beasley-two-bedroom-apartment-with-a-view/1631541147?undefined", geolocator)
+    #geolocator = bing.Bing("AkVuw-CNwasVEvDE8fqHd7qZ2YDO1UTxjkB9UEW7AfKWCJCJpKqInXb83YBH5EoI")
+    #k.Unit().digest_url("https://www.kijiji.ca/v-apartments-condos/nelson/beasley-two-bedroom-apartment-with-a-view/1631541147?undefined", geolocator)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
