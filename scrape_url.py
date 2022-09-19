5d86f04f00681ca0482e66f227c6273e20504267
CaelumD25 <caelumdudek25@gmail.com>
2022-09-19 13:39:21 -0700

    Creating the template for the scraping logic

--- scrape_url.py
+++ scrape_url.py
@@ -1,19 +1,27 @@
 import re
+from bs4 import BeautifulSoup
+import requests
 
 def match_url(url):
-    if re.match(url,""):
+    if re.match(url, r"(https://www\.kijiji\.ca).*"):
+        print("Kijiji Found")
         return 1
-    elif re.match(url,r""):
+    elif re.match(url, r"https://.*?\.(craigslist.org).*"):
+        print("Craigslist Found")
         return 2
-    elif re.match(url,r""):
+    elif re.match(url, r"(https://www\.usedvictoria\.com/).*"):
+        print("Used Victoria Found")
         return 3
-    elif re.match(url,r""):
+    elif re.match(url, r""):
+        print("Facebook Found")
         return 4
     else:
         return 5
 
 
 def scrape_kijiji(url):
+    requested_html = requests.get(url).text
+    soup = BeautifulSoup()
     pass
 
 
